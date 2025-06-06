a8e6df598cfad757bc860a413298a1877b7e14db
Nathalie Ruano <nruano@google.com>
2025-06-06 02:43:03 -0700

    Added changes to expose Phone Number Parsing in Telephony module
    Test: Presubmit
    
    Bug: b/418868405 Bug: b/412560144
    
    Flag: com.android.telephony.flags.phone_number_parsing_api
    
    Change-Id: I17c69c7ee8fbc3c809485a5e4316c073593e9e2a

--- tests/unit/src/com/android/telephony/module/tests/PhoneNumberManagerTest.java
+++ tests/unit/src/com/android/telephony/module/tests/PhoneNumberManagerTest.java
@@ -13,13 +13,220 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.android.telephony.module.tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.junit.Assume.assumeTrue;
+
+import android.content.Context;
+import android.net.Uri;
+import android.platform.test.annotations.RequiresFlagsEnabled;
+import android.telephony.ParsedPhoneNumber;
+import android.telephony.PhoneNumberManager;
+
+import androidx.test.filters.SmallTest;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.android.telephony.flags.Flags;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+@SmallTest
 public class PhoneNumberManagerTest {
+    private PhoneNumberManager mPhoneNumberManager;
+    protected static Context sContext;
+
+    @BeforeClass
+    public static void setStaticFixtures() {
+        if (sContext != null) {
+            return;
+        }
+        try {
+            sContext = InstrumentationRegistry.getInstrumentation().getContext();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Before
+    public void setup() throws Exception {
+        mPhoneNumberManager = sContext.getSystemService(PhoneNumberManager.class);
+        assumeTrue(mPhoneNumberManager != null);
+    }
+
     @Test
-    public void dummyTest() {
+    @RequiresFlagsEnabled(Flags.FLAG_PHONE_NUMBER_PARSING_API)
+    public void testParsingEmptyParametersExceptionThrownWhenFlagEnabled() throws Exception {
+        try {
+            mPhoneNumberManager.parsePhoneNumber(new ArrayList<Uri>(), "");
+            fail("Expected failure due to an invalid argument provided.");
+        } catch (IllegalArgumentException expected) {
+            // expected
+        }
+    }
+
+    @Test
+    @RequiresFlagsEnabled(Flags.FLAG_PHONE_NUMBER_PARSING_API)
+    public void testParsingNullParametersExceptionThrownWhenFlagEnabled() throws Exception {
+        try {
+            mPhoneNumberManager.parsePhoneNumber(null, null);
+            fail("Expected failure due to an invalid argument provided.");
+        } catch (IllegalArgumentException expected) {
+            // expected
+        }
+    }
+
+    @Test
+    @RequiresFlagsEnabled(Flags.FLAG_PHONE_NUMBER_PARSING_API)
+    public void testParsingGlobalNumberWhenGlobalIsExpectedReturnSuccessWhenFlagEnabled() {
+        ParsedPhoneNumber result =
+                mPhoneNumberManager.parsePhoneNumber(
+                    new ArrayList<Uri>(
+                        Arrays.asList(
+                            Uri.parse("tel:+41446681800"),
+                            Uri.parse("sip:+41446681800@ims.mnc260.mcc310.3gppnetwork.org"))),
+                    "CH");
+
+        assertEquals(true, result.isValidPhoneNumber());
+        assertEquals("+41446681800", result.getParsedPhoneNumber());
+        assertEquals(0, result.getErrorCode());
+    }
+
+    @Test
+    @RequiresFlagsEnabled(Flags.FLAG_PHONE_NUMBER_PARSING_API)
+    public void testParsingNationalNumberWhenNationalReturnSuccessWhenFlagEnabled() {
+        ParsedPhoneNumber result =
+                mPhoneNumberManager.parsePhoneNumber(
+                    new ArrayList<Uri>(
+                        Arrays.asList(
+                            Uri.parse("tel:0446681801"),
+                            Uri.parse("sip:0446681801@ims.mnc260.mcc310.3gppnetwork.org"))),
+                    "CH");
+
+        assertEquals(true, result.isValidPhoneNumber());
+        assertEquals("+41446681801", result.getParsedPhoneNumber());
+        assertEquals(0, result.getErrorCode());
+    }
+
+    @Test
+    @RequiresFlagsEnabled(Flags.FLAG_PHONE_NUMBER_PARSING_API)
+    public void testParsingNumberWhenCountryIsoIsInvalidReturnErrorWhenFlagEnabled() {
+        ParsedPhoneNumber result =
+                mPhoneNumberManager.parsePhoneNumber(
+                    new ArrayList<Uri>(
+                        Arrays.asList(
+                            // Guatemalan national format number
+                            Uri.parse("tel:555-1234"),
+                            Uri.parse("sip:555-1234@ims.mnc260.mcc310.3gppnetwork.org"))),
+                    "CH");
+
+        assertEquals(false, result.isValidPhoneNumber());
+        try {
+            result.getParsedPhoneNumber();
+            fail();
+        } catch (IllegalStateException expected) {
+            // expected
+        }
+        assertEquals(1, result.getErrorCode());
+    }
+
+    @Test
+    @RequiresFlagsEnabled(Flags.FLAG_PHONE_NUMBER_PARSING_API)
+    public void testParsingNumberWithInvalidFormatReturnErrorWhenFlagEnabled() {
+        ParsedPhoneNumber result =
+                mPhoneNumberManager.parsePhoneNumber(
+                    new ArrayList<Uri>(
+                        Arrays.asList(
+                            // Invalid number format
+                            Uri.parse("tel:123456789"),
+                            Uri.parse("sip:123456789@ims.mnc260.mcc310.3gppnetwork.org"))),
+                    "CH");
+
+        assertEquals(false, result.isValidPhoneNumber());
+        try {
+            result.getParsedPhoneNumber();
+            fail();
+        } catch (IllegalStateException expected) {
+            // expected
+        }
+        assertEquals(1, result.getErrorCode());
+    }
+
+    @Test
+    @RequiresFlagsEnabled(Flags.FLAG_PHONE_NUMBER_PARSING_API)
+    public void testParsingPhoneDifferentImsNumberFormatReturnSuccessWhenFlagEnabled() {
+        ParsedPhoneNumber result =
+                mPhoneNumberManager.parsePhoneNumber(
+                    new ArrayList<Uri>(
+                        Arrays.asList(
+                            Uri.parse("tel:0446681801"),
+                            Uri.parse("sip:+41446681801@ims.mnc260.mcc310.3gppnetwork.org"))),
+                    "CH");
+
+        assertEquals(true, result.isValidPhoneNumber());
+        assertEquals("+41446681801", result.getParsedPhoneNumber());
+        assertEquals(0, result.getErrorCode());
+    }
+
+    @Test
+    @RequiresFlagsEnabled(Flags.FLAG_PHONE_NUMBER_PARSING_API)
+    public void testParsingNumberWithPlusAndNoCountryCodeReturnErrorWhenFlagEnabled() {
+        ParsedPhoneNumber result =
+                mPhoneNumberManager.parsePhoneNumber(
+                    new ArrayList<Uri>(
+                        Arrays.asList(
+                            Uri.parse("tel:+446681801"),
+                            Uri.parse("sip:+446681801@ims.mnc260.mcc310.3gppnetwork.org"))),
+                    "CH");
+
+        assertEquals(false, result.isValidPhoneNumber());
+        try {
+            result.getParsedPhoneNumber();
+            fail();
+        } catch (IllegalStateException expected) {
+            // expected
+        }
+        assertEquals(1, result.getErrorCode());
+    }
+
+    @Test
+    @RequiresFlagsEnabled(Flags.FLAG_PHONE_NUMBER_PARSING_API)
+    public void testParsingNumberWhenCountryHasDoubleLeadingZerosReturnSuccessWhenFlagEnabled() {
+        ParsedPhoneNumber result =
+                mPhoneNumberManager.parsePhoneNumber(
+                    new ArrayList<Uri>(
+                        Arrays.asList(
+                            Uri.parse("tel:0041446681801"),
+                            Uri.parse("sip:0041446681801@ims.mnc260.mcc310.3gppnetwork.org"))),
+                    "CH");
+
+        assertEquals(true, result.isValidPhoneNumber());
+        assertEquals("+41446681801", result.getParsedPhoneNumber());
+        assertEquals(0, result.getErrorCode());
+    }
+
+
+    @Test
+    @RequiresFlagsEnabled(Flags.FLAG_PHONE_NUMBER_PARSING_API)
+    public void testParsingApiReturnsErrorWhenFlagDisabled() {
+        ParsedPhoneNumber result =
+                mPhoneNumberManager.parsePhoneNumber(
+                    new ArrayList<Uri>(Arrays.asList(Uri.parse("tel:1234567890"))),
+                    "US");
+
+        assertEquals(false, result.isValidPhoneNumber());
+        try {
+            result.getParsedPhoneNumber();
+            fail();
+        } catch (IllegalStateException expected) {
+            // expected
+        }
     }
 }
