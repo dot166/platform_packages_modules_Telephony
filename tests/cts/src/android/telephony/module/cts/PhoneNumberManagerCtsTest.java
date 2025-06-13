b5d250f461e52d9ff2a9ea140e859c1a83a79c12
Hyein Yu <hyeinyu@google.com>
2025-06-13 02:53:26 -0700

    Revert^3 "Fix build error on test_suites_x86_64-next target"
    This reverts commit b2a850358855a4374226311ad5eded0ec206ca5d.
    
    Reason for revert: for check build
    
    Bug: b/424394123
    Bug: b/418868405 Bug: b/412560144
    Test: presubmit
    Change-Id: Ibaea8b9f72b09c40a214e20aa6e6e21e2de9cb3a

--- tests/cts/src/android/telephony/module/cts/PhoneNumberManagerCtsTest.java
+++ tests/cts/src/android/telephony/module/cts/PhoneNumberManagerCtsTest.java
@@ -15,10 +15,114 @@
  */
 package com.android.telephony.module.tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.junit.Assume.assumeTrue;
+
+import android.content.Context;
+import android.net.Uri;
+import android.platform.test.annotations.RequiresFlagsEnabled;
+import android.telephony.ParsedPhoneNumber;
+import android.telephony.PhoneNumberManager;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.android.telephony.flags.Flags;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
 public class PhoneNumberManagerCtsTest {
+    private PhoneNumberManager mPhoneNumberManager;
+    protected static Context sContext;
+
+    @BeforeClass
+    public static void setStaticFixtures() {
+        if (sContext != null) {
+            return;
+        }
+        try {
+            sContext = InstrumentationRegistry.getInstrumentation().getContext();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Before
+    public void setup() {
+        mPhoneNumberManager = sContext.getSystemService(PhoneNumberManager.class);
+        assumeTrue(mPhoneNumberManager != null);
+    }
+
     @Test
-    public void dummyTest() {
+    @RequiresFlagsEnabled(Flags.FLAG_PHONE_NUMBER_PARSING_API)
+    public void testInvalidParametersThrowsExceptionWhenFlagEnabled() {
+        try {
+            ParsedPhoneNumber result =
+                    mPhoneNumberManager.parsePhoneNumber(new ArrayList<Uri>(), null);
+            fail("Expected failure due to an invalid argument provided.");
+        } catch (IllegalArgumentException expected) {
+            // expected
+        }
+    }
+
+    @Test
+    @RequiresFlagsEnabled(Flags.FLAG_PHONE_NUMBER_PARSING_API)
+    public void testParsingNumberWithInvalidFormatReturnErrorWhenFlagEnabled() {
+        ParsedPhoneNumber result =
+                mPhoneNumberManager.parsePhoneNumber(
+                    new ArrayList<>(
+                        Arrays.asList(
+                            // Invalid number format
+                            Uri.parse("tel:123456789"),
+                            Uri.parse("sip:123456789@ims.mnc260.mcc310.3gppnetwork.org"))),
+                    "CH");
+
+        assertEquals(false, result.isValidPhoneNumber());
+        try {
+            result.getParsedPhoneNumber();
+            fail();
+        } catch (IllegalStateException expected) {
+            // expected
+        }
+        assertEquals(1, result.getErrorCode());
+    }
+
+    @Test
+    @RequiresFlagsEnabled(Flags.FLAG_PHONE_NUMBER_PARSING_API)
+    public void testValidParametersReturnsSuccessWhenFlagEnabled() throws Exception {
+        ParsedPhoneNumber result =
+                mPhoneNumberManager.parsePhoneNumber(
+                    new ArrayList<Uri>(
+                        Arrays.asList(
+                            Uri.parse("tel:0446681801"),
+                            Uri.parse("sip:0446681801@ims.mnc260.mcc310.3gppnetwork.org"))),
+                    "CH");
+
+        assertEquals(true, result.isValidPhoneNumber());
+        assertEquals("+41446681801", result.getParsedPhoneNumber());
+        assertEquals(0, result.getErrorCode());
+    }
+
+    @Test
+    @RequiresFlagsEnabled(Flags.FLAG_PHONE_NUMBER_PARSING_API)
+    public void testParsingApiReturnsErrorWhenFlagDisabled() {
+        ParsedPhoneNumber result =
+                mPhoneNumberManager.parsePhoneNumber(
+                    new ArrayList<>(Arrays.asList(Uri.parse("tel:1234567890"))),
+                    "US");
+
+        assertEquals(false, result.isValidPhoneNumber());
+        try {
+            result.getParsedPhoneNumber();
+            fail();
+        } catch (IllegalStateException expected) {
+            // expected
+        }
     }
 }
